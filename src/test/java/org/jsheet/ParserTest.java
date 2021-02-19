14a8b20a6a14a3d0e0b498e55207ab1c8e514b31
Dmitrii Suvorov <wotopul@gmail.com>
2021-02-19 21:24:21 +0300

    Fix parser tests

--- src/test/java/org/jsheet/ParserTest.java
+++ src/test/java/org/jsheet/ParserTest.java
@@ -19,25 +19,25 @@ public class ParserTest {
 
     @Test
     public void simpleExpression1() {
-        ExprWrapper expr = ParserUtils.parse("1 + 2 * 3");
+        ExprWrapper expr = ParserUtils.parse("= 1 + 2 * 3");
         assertEquals(1 + 2 * 3, expr.eval(model).get(), 0);
     }
 
     @Test
     public void simpleExpression2() {
-        ExprWrapper expr = ParserUtils.parse("(1 + 2) * 3");
+        ExprWrapper expr = ParserUtils.parse("= (1 + 2) * 3");
         assertEquals((1 + 2) * 3, expr.eval(model).get(), 0);
     }
 
     @Test
     public void simpleExpression3() {
-        ExprWrapper expr = ParserUtils.parse("pow(2, 4)");
+        ExprWrapper expr = ParserUtils.parse("= pow(2, 4)");
         assertEquals(Math.pow(2, 4), expr.eval(model).get(), 0);
     }
 
     @Test
     public void simpleExpression4() {
-        ExprWrapper expr = ParserUtils.parse("abracadabra(2, 4)");
+        ExprWrapper expr = ParserUtils.parse("= abracadabra(2, 4)");
         Result result = expr.eval(model);
         assertFalse(result.isPresent());
         assertEquals("Unknown function: abracadabra", result.message());
